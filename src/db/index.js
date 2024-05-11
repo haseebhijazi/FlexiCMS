a51bf483e5bbc39be90df126039754d2268f5164
haseebhijazi <haseebk841@gmail.com>
2024-05-11 12:34:45 +0530

    SQL connection established

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,42 @@
-import mongoose from "mongoose";
+import mysql from 'mysql2/promise'
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+        const pool = await mysql.createPool({
+            host: `${process.env.DB_HOST}`,
+            user: `${process.env.DB_USER}`,
+            password: `${process.env.DB_PASSWORD}`,
+            database: `${DB_NAME}`
+        })
+        return pool
+        // const grantSql = `GRANT CREATE ON *.* TO ${process.env.DB_USER}@${process.env.DB_HOST};`
+        // await pool.execute(grantSql)
     } catch (error) {
-        console.log("MONGODB Connection FAILED: ", error)
-        process.exit(1)
+        console.log("🌐 MySQL Connection Failed:", error)
+        throw error
     }
 }
 
-export default connectDB
+const createDB = async (pool, databaseName) => {
+    try {
+        await pool.execute(`CREATE DATABASE IF NOT EXISTS ${databaseName}`)
+        console.log(`MySQL Database '${databaseName}' created successfully.`)
+    } catch (error) {
+        console.error("MySQL Error creating database:", error)
+        throw error
+    }
+}
+
+const setupDB = async () => {
+    try {
+        const pool = await connectDB()
+        await createDB(pool, `${ DB_NAME }`)
+        return pool
+    } catch (error) {
+        console.error("MySQL Database Setup Failed: ", error)
+        throw error
+    }
+}
+
+export default setupDB
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-
+import setupDB from "./db/index.js";
+import {app} from "./app.js"
 dotenv.config({
     path: './env'
 })
 
-connectDB()
+async function startServer() {
+    try {
+        const pool = await setupDB();
+
+        app.on("error", (error) => {
+            console.log("⚙️ Server Error: ", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`⚙️ Server is running at port: ${process.env.PORT}`);
+        });
+
+
+        /*
+                    ================ QUERY ==================
+        // const connection = await pool.getConnection();
+        // const [rows] = await connection.query(`SELECT * FROM Persons;`);
+        // console.log(rows);
+        // connection.release();
+                    =========================================
+        */
+    } catch (error) {
+        console.log("🌐 MySQL Connection FAILED <src/index.js> : ", error)
+    }
+}
+
+startServer();
